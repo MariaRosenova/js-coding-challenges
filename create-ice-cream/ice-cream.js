73df89fd857a7f8e9b318a9d4218f4d5450ecfeb
Maria <mimito1353@gmail.com>
2023-09-23 19:28:21 +0300

    This commit refactors the code to use async/await for improved readability and maintainability.

--- create-ice-cream/ice-cream.js
+++ create-ice-cream/ice-cream.js
@@ -7,46 +7,43 @@ let stocks = {
 
 let is_shop_open = true;
 
-function order(time, work) {
+async function kitchen() {
+  try {
+    await time(1000);
+    console.log(`${stocks.Fruits[0]} was selected.`);
+
+    await time(2000);
+    console.log("fruit has been chopped.");
+
+    await time(1000);
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added.`);
+
+    await time(1000);
+    console.log("start the machine.");
+
+    await time(2000);
+    console.log(`ice cream placed on ${stocks.holder[1]}`);
+
+    await time(3000);
+    console.log(`${stocks.toppings[0]} as toppings.`);
+
+    await time(2000);
+    console.log("Serve the Ice Cream");
+  } catch (error) {
+    console.log("Customer left", error);
+  } finally {
+    console.log("Day ended, shop closed.");
+  }
+}
+
+kitchen();
+
+function time(ms) {
   return new Promise((resolve, reject) => {
     if (is_shop_open) {
-      setTimeout(() => {
-        resolve(work());
-      }, time);
+      setTimeout(resolve, ms);
     } else {
-      reject(console.log("Our shop is closed."));
+      reject(console.log("Shop is closed"));
     }
   });
 }
-order(1000, () => console.log(`${stocks.Fruits[0]} was selected.`))
-  .then(() => {
-    return order(1000, () => console.log("Production has started."));
-  })
-  .then(() => {
-    return order(2000, () => console.log("Fruit has been chopped."));
-  })
-  .then(() => {
-    return order(1000, () =>
-      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`)
-    );
-  })
-  .then(() => {
-    return order(1000, () => console.log("Start the machine"));
-  })
-  .then(() => {
-    return order(2000, () =>
-      console.log(`Ice cream placed on ${stocks.holder[1]}`)
-    );
-  })
-  .then(() => {
-    return order(3000, () => console.log(`${stocks.toppings[0]} as toppings`));
-  })
-  .then(() => {
-    return order(2000, () => console.log("Serve Ice Cream. :)"));
-  })
-  .catch(() => {
-    console.log("Customer left.");
-  })
-  .finally(() => {
-    console.log("End of Day.");
-  });
